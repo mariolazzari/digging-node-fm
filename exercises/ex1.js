ea1b587c2b7f5ead741ddeaf357dbda8a9e7ce89
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-30 16:22:45 +0100

    Started ex1

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,9 +1,15 @@
-// standard input:
-console.log("Hello World!");
-process.stdout.write("Hello World!\n");
+#!/usr/bin/env node
 
-// standard error:
-console.error("Ooops!");
+"use strict";
 
-// node ex1.js 1 > /dev/null
-// node ex1.js 2 > /dev/null
+// ************************************
+
+function printHelp() {
+  console.log("ex1 usage:");
+  console.log("");
+  console.log("--help                      print this help");
+  console.log("-, --in                     read file from stdin");
+  console.log("--file={FILENAME}           read file from {FILENAME}");
+  console.log("");
+  console.log("");
+}
