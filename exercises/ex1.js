f913ab70830eace4103f5b890cca4cb7c90385fd
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-30 15:21:53 +0100

    Started ex1

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,1 +1,9 @@
-// My first Node.js program
+// standard input:
+console.log("Hello World!");
+process.stdout.write("Hello World!\n");
+
+// standard error:
+console.error("Ooops!");
+
+// node ex1.js 1 > /dev/null
+// node ex1.js 2 > /dev/null
